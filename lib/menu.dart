8d58d9b7d93fd02e4f3bd024fe2f482c779f7075
marcelomosquiari <marcelo.mosquiari@fatec.sp.gov.br>
2020-06-22 19:32:06 -0300

    Update menu.dart

--- lib/menu.dart
+++ lib/menu.dart
@@ -1,134 +1,65 @@
 import 'package:flutter/material.dart';
-import 'package:projeto_p1/page_login.dart';
-import 'package:projeto_p1/profissionais.dart';
-import 'package:projeto_p1/contato.dart';
-import 'package:projeto_p1/descricao.dart';
-import 'package:projeto_p1/cadastro.dart';
 
 
-
-
-class MenuApp extends StatelessWidget {
-  static BuildContext scaffoldContext;
-  static String tag = 'menu';
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Menu do Aplicativo",
-      home: MyStatefulWidget(),
-      
-    );
-     
-  }
-}
- 
-
-class MyStatefulWidget extends StatefulWidget {
-  MyStatefulWidget({Key key}) : super(key: key);
-
-  @override
-  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-
-
-int _selectedIndex = 0;
-int currItem = 0;
-static final pages = [
-    Cadastro.tag,
-    Profissionais.tag,
-    Contato.tag,
-    Descricao.tag,    
-    LoginPage.tag
-  ];
-
- 
- 
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Cadastro de Profissionais',
-      style: optionStyle,
-    ),
-    Text(
-      'Profissionais Cadastrados',
-      style: optionStyle,
-    ),
-    Text(
-      'Contato: Fale Conosco',
-      style: optionStyle,
-    ),
-    Text(
-      'Descrição do Aplicativo',
-      style: optionStyle,
-    ),
-    Text(
-      'Sair',
-      style: optionStyle,
-    ),
-  ];
-
-   
+class Menuapp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Menu do Aplicativo'),
-      ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
+        title: Text("Menu do Aplicativo"),
+        backgroundColor: Colors.grey,
+        
+        
+        automaticallyImplyLeading: false,
+        
       ),
       
-      
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 0,
-        items: [
-          
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person_add),
-            title: Text('Cadastro'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.list),
-            title: Text('Profissionais Cadastrados'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.contact_mail),
-            title: Text('Contato'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.description),
-            title: Text('Descrição'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.exit_to_app),
-            title: Text('Sair'),
-          ),  
-        ],
-        
-        selectedItemColor: Colors.black,
-        unselectedItemColor: Colors.grey,
+      body: Container(
         
         
+       
+        
+        
+        child: ListView(    
                 
-        onTap: (int i){
-        currItem = i;
-        Navigator.of(context).pushNamed(pages[i]);
-        }
+          
+          
+        children: <Widget>[
+          DrawerHeader(
+            child: Image(image: AssetImage('assets/imagens/app-logo.png')),
+            
+              
+            ),
+            
+          ListTile(
+            leading: Icon(Icons.person_add),
+            title: Text('Cadastro de Profissionais'),
+            onTap: () => {Navigator.of(context).pushNamed('/pagina3')},
+          ),
+          ListTile(
+            leading: Icon(Icons.list),
+            title: Text('Lista de Profissionais Cadastrados'),
+            onTap: () => {Navigator.of(context).pushNamed('/pagina4')},
+          ),
+          ListTile(
+            leading: Icon(Icons.contact_mail),
+            title: Text('Contato: Fale Conosco'),
+            onTap: () => {Navigator.of(context).pushNamed('/pagina5')},
+          ),
+          ListTile(
+            leading: Icon(Icons.description),
+            title: Text('Descrição do Aplicativo'),
+            onTap: () => {Navigator.of(context).pushNamed('/pagina6')},
+          ),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text('Sair'),
+            onTap: () => {Navigator.of(context).pushNamed('/pagina1')},
+          ),
+        ],
       ),
-
-      
-      
+    ), 
     );
-  }
-  
-}
-
-
-
-
-
+  }        
+ }
